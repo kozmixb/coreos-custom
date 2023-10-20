ad9b644b1a88c5a563b69b27d5037c3631299d2b
Bence Kozma <bencejob@gmail.com>
2023-10-20 22:07:29 +0100

    add script

---
+++ script/kubernetes.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Run on master node
+sudo rpm-ostree install kubelet kubeadm kubectl cri-o
+sudo systemctl enable --now crio kubelet
+
+# Run this on slaves
+# sudo rpm-ostree install kubelet kubeadm cri-o
+
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+kubectl apply -f \
+https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml

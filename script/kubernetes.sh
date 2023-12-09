992bfea51ced28182e8165d8c154afc56cd9d842
Bence Kozma <46692466+kozmixb@users.noreply.github.com>
2023-12-09 16:38:42 +0000

    Update kubernetes.sh

--- script/kubernetes.sh
+++ script/kubernetes.sh
@@ -19,5 +19,4 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 
-kubectl apply -f \
-https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
